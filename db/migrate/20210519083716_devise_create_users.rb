c28c60bffd7f29828bac524202bc09ba646c72b6
Katsuaki-T <cxq84403@gmail.com>
2021-05-19 17:52:35 +0900

    user migration file

--- db/migrate/20210519083716_devise_create_users.rb
+++ db/migrate/20210519083716_devise_create_users.rb
@@ -4,8 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :birth_country,      null: false
+      t.integer :prefecture_id,     null: false
+      t.string :identity  
 
       ## Recoverable
       t.string   :reset_password_token
