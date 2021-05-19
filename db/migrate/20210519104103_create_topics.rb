195d829fec82605eaa08701e0695ef97d582fd9d
Katsuaki-T <cxq84403@gmail.com>
2021-05-19 19:58:14 +0900

    topic migration

--- db/migrate/20210519104103_create_topics.rb
+++ db/migrate/20210519104103_create_topics.rb
@@ -2,6 +2,10 @@ class CreateTopics < ActiveRecord::Migration[6.1]
   def change
     create_table :topics do |t|
 
+      t.integer :sentence_id,          null: false
+      t.text :description,           null: false
+      t.references :user,         foreign_key: true
+      
       t.timestamps
     end
   end
