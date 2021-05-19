b53d02ef1b4f653e919d48d87af5888d36b8ee7f
Katsuaki-T <cxq84403@gmail.com>
2021-05-19 18:05:04 +0900

    user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
+  has_many :topics
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
-  
+
 end
