9102720557240d4c51c364df006c9403c7d0c676
Katsuaki-T <cxq84403@gmail.com>
2021-05-19 18:13:23 +0900

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+  validates :name
+  validates :birth_country
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
 
-  has_many :topics
   
+  has_many :topics
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 
