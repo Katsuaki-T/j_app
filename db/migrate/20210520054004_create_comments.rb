b1f81aeef7ef041386e1c32130f6e22d199dfd0e
Katsuaki-T <cxq84403@gmail.com>
2021-05-20 14:45:05 +0900

    column

--- db/migrate/20210520054004_create_comments.rb
+++ db/migrate/20210520054004_create_comments.rb
@@ -2,6 +2,9 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
 
+      t.text :text,              null: false
+      t.references :topic,       foreign_key: true
+
       t.timestamps
     end
   end
