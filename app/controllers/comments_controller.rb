2df8adfcb62fae5aa4978475e8e938b82c4b505b
Katsuaki-T <cxq84403@gmail.com>
2021-05-20 15:36:18 +0900

    comment_params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,4 +11,10 @@ class CommentsController < ApplicationController
     end
   end
 
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, topic_id: params[:topic_id])
+  end
+
 end
