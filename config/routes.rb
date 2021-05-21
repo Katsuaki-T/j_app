081d7ab923f21014081e3a45e5072918b563166d
Katsuaki-T <cxq84403@gmail.com>
2021-05-21 16:52:49 +0900

    route search

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,13 @@ Rails.application.routes.draw do
 
  resources :topics do
   resources :comments, only: [:create]
+  collection do
+    get 'search'
+  end
  end
 
 resources :kind_japanese, only: [:index]
+
+
 
 end
