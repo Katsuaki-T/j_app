bfce697ab9fbc4dc5746f29cd4ee278cc44dd0a3
Katsuaki-T <cxq84403@gmail.com>
2021-05-23 10:40:06 +0900

    bundle  rspec /comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe Topic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Comment, type: :model do
+  describe '#comment ability' do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    context '内容に問題がない場合' do
+      it 'ユーザーとトピックに紐づいたコメントがあればコメントできる場合' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'コメントがないと保存できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("文章（ぶんしょう）を入力してください")
+      end
+
+      it 'userが紐付いていないと保存できないこと' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Userを入力してください')
+      end
+      
+      it 'topicが紐付いていないと保存できないこと' do
+        @comment.topic = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Topicを入力してください')
+      end
+
+    end
+  end
 end
