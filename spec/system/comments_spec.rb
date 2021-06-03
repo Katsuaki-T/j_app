fde083eb3c41c5fa9dca28f9d3c55698441c2f1b
Katsuaki-T <cxq84403@gmail.com>
2021-06-03 14:07:46 +0900

    comment sign_in(@user)

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Comments", type: :system do
   before do
+    @user = FactoryBot.create(:user)
     @topic = FactoryBot.create(:topic)
     @comment = Faker::Lorem.sentence
   end
 
   it 'ログインしたユーザーはツイート詳細ページでコメント投稿できる' do
     # ログインする
+    sign_in(@user)
     # topic詳細ページに遷移する
     # フォームに情報を入力する
     # コメントを送信すると、Commentモデルのカウントが1上がることを確認する
