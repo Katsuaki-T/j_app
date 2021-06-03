ea682f5954a076d7ca13ea980de42912bbee67b9
Katsuaki-T <cxq84403@gmail.com>
2021-06-03 14:43:43 +0900

    comment test

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -11,9 +11,14 @@ RSpec.describe "Comments", type: :system do
     # ログインする
     sign_in(@user)
     # topic詳細ページに遷移する
+    visit topic_path(@topic)
     # フォームに情報を入力する
+    fill_in 'comment[text]', with: @comment
     # コメントを送信すると、Commentモデルのカウントが1上がることを確認する
-    
+    expect{
+      find('button[name="commit"]').click
+    }.to change { Comment.count }.by(1)
     # 詳細ページ上に先ほどのコメント内容が含まれていることを確認する
+    expect(page).to have_content @comment
   end
 end
